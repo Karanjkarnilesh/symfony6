a32eba4b5b703c25e7dbfe466a365e335ca396da
karanjkarnilesh <karanjkar.nk@gmail.com>
2022-09-02 18:25:01 +0530

    twig templates added

--- src/Controller/VinyController.php
+++ src/Controller/VinyController.php
@@ -2,14 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinyController{
+class VinyController extends AbstractController{
     #[Route('/')]
     public function homepage()
     {
-        return new Response('Hello');
+        $tracks=[
+            ['song' =>'chal sath sath','artist' =>" nilesh"],
+            ['song' =>'ami shetkari','artist' =>"nandkishor"],
+            ['song' =>'ham sath sath hai','artist' =>" pragati"],
+        ];
+        return $this->render('viny/homepage.html.twig',[
+            'title'=>'PB & Jams',
+            'tracks' =>$tracks,
+        ]);
     }
     #[Route('/browse/{slug}')]
     public function browse(string $slug =null) :Response
